655eda878643ce7bdf6036bff76993a34e10bc0b
Claudio Roberto da Silva <108074829+ArkhosAggelos@users.noreply.github.com>
2025-07-01 20:28:41 -0300

    Update listener.js

--- api/listener.js
+++ api/listener.js
@@ -6,13 +6,10 @@ dotenv.config();
 const { Client } = pkg;
 
 export default async function handler(req, res) {
-  const db = new Client({
-    connectionString: process.env.PG_URL,
-    ssl: { rejectUnauthorized: false }
-  });
+  const db = new Client({ connectionString: process.env.PG_URL, ssl: { rejectUnauthorized: false } });
   await db.connect();
 
-  const client = mqtt.connect({
+  const mqttClient = mqtt.connect({
     host: process.env.MQTT_BROKER,
     port: parseInt(process.env.MQTT_PORT),
     protocol: "mqtts",
@@ -20,21 +17,19 @@ export default async function handler(req, res) {
     password: process.env.MQTT_PASSWORD
   });
 
-  client.on("connect", () => {
-    client.subscribe("estacao/externo");
+  mqttClient.on("connect", () => {
+    mqttClient.subscribe("estacao/externo");
   });
 
-  client.on("message", async (topic, message) => {
+  mqttClient.on("message", async (_, message) => {
     const { id, temperatura, umidade, pressao, lux, previsao } = JSON.parse(message.toString());
+    await db.query(`
+      INSERT INTO leituras (id, temperatura, umidade, pressao, lux, previsao)
+      VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (id) DO NOTHING
+    `, [id, temperatura, umidade, pressao, lux, previsao]);
 
-    await db.query(
-      `INSERT INTO leituras (id, temperatura, umidade, pressao, lux, previsao)
-       VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (id) DO NOTHING`,
-      [id, temperatura, umidade, pressao, lux, previsao]
-    );
-    client.end();
-    db.end();
-    res.status(200).send("Dados salvos com sucesso!");
+    mqttClient.end();
+    await db.end();
+    res.status(200).send("Dados inseridos!");
   });
 }
-
